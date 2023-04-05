3ef6b7267f90dbf862be80383ba153cd12aebe2b
Ali Asghar <aliasghar.chakera@gmail.com>
2023-04-05 13:36:59 +0500

    Completed

--- SOM/input.py
+++ SOM/input.py
@@ -1,15 +1,21 @@
 import pandas as pd
-from SOM.SOM import SOM
+from SOM import SOM
+from sklearn.preprocessing import MinMaxScaler
 
 def readData(path):
     # Read in the CSV file using pandas
     df = pd.read_csv(path)
     # Drop any rows that have missing data
     df.dropna(inplace=True)
-    # Extract the columns you want to use as inputs for the SOM
-    data = df[["Population", "Area (sq. mi.)", "GDP ($ per capita)", "Literacy (%)", "Phones (per 1000)", "Pop. Density (per sq. mi.)", "Agriculture", "Industry", "Service"]].to_numpy()
-    # Convert the countries to a list
     countries = df[["Country"]]["Country"].values.tolist()
+    # df.drop(columns=["Country"], inplace=True)
+    # Extract the columns you want to use as inputs for the SOM
+    data = df[["Population", "Area (sq. mi.)", "GDP ($ per capita)", "Literacy (%)", "Phones (per 1000)", "Pop. Density (per sq. mi.)", "Agriculture", "Industry", "Service"]]
+    # print(data)
+    # Convert the countries to a list
+    scaler = MinMaxScaler()
+    df = data.copy()
+    data = pd.DataFrame(scaler.fit_transform(df), columns=df.columns).to_numpy()
     return data, countries
 data, countries = readData('countries of the world.csv')
 # Create a SOM object
